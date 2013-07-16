dd814af651ac17653aee71d96f06e85d4efc3161
Bradd Szonye <bszonye@gmail.com>
2013-07-16 03:58:05 -0700

    Improve color handling.

--- xterm_colour_chart.py
+++ xterm_colour_chart.py
@@ -104,20 +104,21 @@ cloud_shape_88 = """
 """
 
 boxes = """
-a00a01a02a03a04a05   c00c01c02c03c04c05   e00e01e02e03e04e05   +01+24   .00.08
-a10a11a12a13a14a15   c10c11c12c13c14c15   e10e11e12e13e14e15   +02+23   .01.09
-a20a21a22a23a24a25   c20c21c22c23c24c25   e20e21e22e23e24e25   +03+22   .02.10
-a30a31a32a33a34a35   c30c31c32c33c34c35   e30e31e32e33e34e35   +04+21   .03.11
-a40a41a42a43a44a45   c40c41c42c43c44c45   e40e41e42e43e44e45   +05+20   .04.12
-a50a51a52a53a54a55   c50c51c52c53c54c55   e50e51e52e53e54e55   +06+19   .05.13
-                                                               +07+18   .06.14
-b00b01b02b03b04b05   d00d01d02d03d04d05   f00f01f02f03f04f05   +08+17   .07.15
-b10b11b12b13b14b15   d10d11d12d13d14d15   f10f11f12f13f14f15   +09+16
-b20b21b22b23b24b25   d20d21d22d23d24d25   f20f21f22f23f24f25   +10+15
-b30b31b32b33b34b35   d30d31d32d33d34d35   f30f31f32f33f34f35   +11+14
-b40b41b42b43b44b45   d40d41d42d43d44d45   f40f41f42f43f44f45   +12+13
-b50b51b52b53b54b55   d50d51d52d53d54d55   f50f51f52f53f54f55
+a00a01a02a03a04a05   b00b01b02b03b04b05   c00c01c02c03c04c05
+a10a11a12a13a14a15   b10b11b12b13b14b15   c10c11c12c13c14c15
+a20a21a22a23a24a25   b20b21b22b23b24b25   c20c21c22c23c24c25
+a30a31a32a33a34a35   b30b31b32b33b34b35   c30c31c32c33c34c35
+a40a41a42a43a44a45   b40b41b42b43b44b45   c40c41c42c43c44c45
+a50a51a52a53a54a55   b50b51b52b53b54b55   c50c51c52c53c54c55   .00.08
+                                                               .01.09
+d00d01d02d03d04d05   e00e01e02e03e04e05   f00f01f02f03f04f05   .02.10
+d10d11d12d13d14d15   e10e11e12e13e14e15   f10f11f12f13f14f15   .03.11
+d20d21d22d23d24d25   e20e21e22e23e24e25   f20f21f22f23f24f25   .04.12
+d30d31d32d33d34d35   e30e31e32e33e34e35   f30f31f32f33f34f35   .05.13
+d40d41d42d43d44d45   e40e41e42e43e44e45   f40f41f42f43f44f45   .06.14
+d50d51d52d53d54d55   e50e51e52e53e54e55   f50f51f52f53f54f55   .07.15
 
++01+02+03+04+05+06+07+08+09+10+11+12+13+14+15+16+17+18+19+20+21+22+23+24
 """
 
 
@@ -261,15 +262,47 @@ cube_start = 16 # first index of colour cube
 cube_size = 6 # one side of the colour cube
 gray_start = cube_size ** 3 + cube_start
 colours = 256
-# Values from: http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
-cube_steps = 0x00, 0x5f, 0x87, 0xaf, 0xd7, 0xff
+# Values from mintty/wintext.c and xterm/256colres.h:
+cube_steps = (0x00, 0x5f, 0x87, 0xaf, 0xd7, 0xff)
 gray_steps = (0x08, 0x12, 0x1c, 0x26, 0x30, 0x3a, 0x44, 0x4e, 0x58, 0x60,
-    0x66, 0x76, 0x80, 0x8a, 0x94, 0x9e, 0xa8, 0xb2, 0xbc, 0xc6, 0xd0,
-    0xda, 0xe4, 0xee)
-basic_colours = ((0,0,0), (128, 0, 0), (0, 128, 0), (128, 128, 0),
-    (0, 0, 128), (128, 0, 128), (0, 128, 128), (192, 192, 192),
-    (128, 128, 128), (255, 0, 0), (0, 255, 0), (255, 255, 0),
-    (0, 0, 255), (255, 0, 255), (0, 255, 255), (255, 255, 255))
+        0x66, 0x76, 0x80, 0x8a, 0x94, 0x9e, 0xa8, 0xb2, 0xbc, 0xc6, 0xd0,
+        0xda, 0xe4, 0xee)
+if 1:
+    # Values from mintty/config.c:
+    basic_colours = (
+            (0x00, 0x00, 0x00), (0xbf, 0x00, 0x00),
+            (0x00, 0xbf, 0x00), (0xbf, 0xbf, 0x00),
+            (0x00, 0x00, 0xbf), (0xbf, 0x00, 0xbf),
+            (0x00, 0xbf, 0xbf), (0xbf, 0xbf, 0xbf),
+            (0x40, 0x40, 0x40), (0xff, 0x40, 0x40),
+            (0x40, 0xff, 0x40), (0xff, 0xff, 0x40),
+            (0x60, 0x60, 0xff), (0xff, 0x40, 0xff),
+            (0x40, 0xff, 0xff), (0xff, 0xff, 0xff))
+elif 0:
+    # Values from X11/rgb.txt and XTerm-col.ad:
+    basic_colours = (
+            (0x00, 0x00, 0x00), # black
+            (0xcd, 0x00, 0x00), # red3
+            (0x00, 0xcd, 0x00), # green3
+            (0xcd, 0xcd, 0x00), # yellow3
+            (0x00, 0x00, 0xee), # blue2
+            (0xcd, 0x00, 0xcd), # magenta3
+            (0x00, 0xcd, 0xcd), # cyan3
+            (0xe5, 0xe5, 0xe5), # gray90
+            (0x7f, 0x7f, 0x7f), # gray50
+            (0xff, 0x00, 0x00), # red
+            (0x00, 0xff, 0x00), # green
+            (0xff, 0xff, 0x00), # yellow
+            (0x5c, 0x5c, 0xff), # rgb:5c/5c/ff
+            (0xff, 0x00, 0xff), # magenta
+            (0x00, 0xff, 0xff), # cyan
+            (0xff, 0xff, 0xff)) # white
+elif 0:
+    # Values from http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
+    basic_colours = ((0,0,0), (128, 0, 0), (0, 128, 0), (128, 128, 0),
+            (0, 0, 128), (128, 0, 128), (0, 128, 128), (192, 192, 192),
+            (128, 128, 128), (255, 0, 0), (0, 255, 0), (255, 255, 0),
+            (0, 0, 255), (255, 0, 255), (0, 255, 255), (255, 255, 255))
 
 def set_88_colour_mode():
     """Switch to 88-colour mode."""
@@ -452,7 +485,7 @@ def draw_chart(chart, origin, angle, hexadecimal, decimal, urwidmal, cell_cols,
         if not any((hexadecimal, decimal, urwidmal)) or row!=cell_rows-1:
             return "\x1b[48;5;%dm%s" % (n, cell_pad)
         # Use a contrasting foreground color.
-        fg = "37" if n_to_gray(n) <= n_to_gray(8) else "30"
+        fg = "37" if n_to_gray(n) < 0x40 else "30"
         if hexadecimal:
             return "\x1b[48;5;%d;%sm%02x%s" % (n, fg, n, cell_pad[2:])
         elif decimal:
